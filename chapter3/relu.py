d32e4513b3a64af9803d38b911a798c1a5658ce1
ishijimaitsuki <tatsuki445@gmail.com>
2020-09-07 21:43:56 +0900

    ReLU関数

---
+++ chapter3/relu.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+if __name__ == '__main__':
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = relu(x)
+    plt.plot(x, y)
+    plt.ylim(-1.0, 5.5)
+    plt.show()
