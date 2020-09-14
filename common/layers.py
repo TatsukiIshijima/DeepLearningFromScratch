4fffd9ceabdac3d7743ad7159c560216d2287bf9
ishijimaitsuki <tatsuki445@gmail.com>
2020-09-14 21:31:08 +0900

    ソフトマックス-損失関数レイヤ

--- common/layers.py
+++ common/layers.py
@@ -1,4 +1,5 @@
-import numpy as np
+from common.functions import *
+
 
 class Relu:
     """
@@ -68,4 +69,35 @@ class Affine:
         self.db = np.sum(dout, axis=0)
         # 入力データの形状を戻す（テンソル対応）
         dx = dx.reshape(*self.original_x_shape)
+        return dx
+
+
+class SoftmaxWithLoss:
+    """
+    ソフトマックス-損失関数レイヤ
+    """
+
+    def __init__(self):
+        # 損失
+        self.loss = None
+        # softmaxの出力
+        self.y = None
+        # 教師データ
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self):
+        batch_size = self.t.shape[0]
+        # 教師データが one-hot-vector の場合
+        if self.t.size == self.y.size:
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
         return dx
