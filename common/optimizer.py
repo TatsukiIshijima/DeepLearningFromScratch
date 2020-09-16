59ad099a3578c7403aeca912c05502b76276b8d3
ishijimaitsuki <tatsuki445@gmail.com>
2020-09-16 21:17:05 +0900

    AdaGrad

--- common/optimizer.py
+++ common/optimizer.py
@@ -22,7 +22,8 @@ class SGD:
 
 class Momentum:
     """
-    モーメンタム
+    モーメンタム SGD
+    物理法則的に最適化する方法
     """
     def __init__(self, lr=0.01, momentum=0.9):
         """
@@ -46,3 +47,32 @@ class Momentum:
         for key in params.keys():
             self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
             params[key] += self.v[key]
+
+
+class AdaGrad:
+    """
+    AdaGrad
+    各パラメータに対してオーダーメードの値を作成し、最適化する方法
+    """
+    def __init__(self, lr=0.01):
+        """
+        :param lr: 学習係数
+        """
+        self.lr = lr
+        self.h = None
+
+    def update(self, params, grads):
+        """
+        :param params: 重みパラメータ（複数）
+        :param grads: 勾配（複数）
+        :return:
+        """
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            # self.h[key] に 0　があった場合に 0 で除算しないよう 1e-7 を追加
+            params[key] -= self.lr * grads[key] * (np.sqrt(self.h[key] + 1e-7))
