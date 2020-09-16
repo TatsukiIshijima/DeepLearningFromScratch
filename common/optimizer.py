6d753cfbffb3af0840b437a7c4aff272cd3da75e
ishijimaitsuki <tatsuki445@gmail.com>
2020-09-16 21:24:14 +0900

    Adam

--- common/optimizer.py
+++ common/optimizer.py
@@ -76,3 +76,38 @@ class AdaGrad:
             self.h[key] += grads[key] * grads[key]
             # self.h[key] に 0　があった場合に 0 で除算しないよう 1e-7 を追加
             params[key] -= self.lr * grads[key] * (np.sqrt(self.h[key] + 1e-7))
+
+
+class Adam:
+    """
+    Adam
+    Momentum と AdaGrad を混ぜ合わせたような最適化手法
+    """
+    def __init__(self, lr=0.01, beta1=0.9, beta2=0.999):
+        """
+        :param lr: 学習係数
+        :param beta1: １次モーメント係数
+        :param beta2: 2次モーメント係数
+        """
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
+
+    def update(self, params, grads):
+        if self.m is None:
+            self.m, self.v = {}, {}
+            for key, val in params.items():
+                self.m[key] = np.zeros_like(val)
+                self.v[key] = np.zeros_like(val)
+
+        self.iter += 1
+        lr_t = self.lr * np.sqrt(1.0 - self.beta2 * self.iter) / (1.0 - self.beta1**self.iter)
+
+        for key in params.keys():
+            self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
+            self.v[key] += (1 - self.beta2) * (grads[key]**2 - self.v[key])
+
+            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
