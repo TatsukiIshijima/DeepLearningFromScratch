43354bc67a789ce75749e40a3541f3a1a8495752
ishijimaitsuki <tatsuki445@gmail.com>
2020-10-03 18:35:39 +0900

    Convolutionレイヤ

--- common/layers.py
+++ common/layers.py
@@ -1,4 +1,5 @@
 from common.functions import *
+from common.util import im2col, col2im
 
 
 class Relu:
@@ -204,5 +205,64 @@ class BatchNormalization:
 
         self.dgamma = dgamma
         self.dbeta = dbeta
+
+        return dx
+
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        """
+        :param W: フィルター（FN, C, FH, FW）
+        :param b: バイアス（FN, 1, 1）
+        :param stride: ストライド
+        :param pad: パディング
+        """
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+        # 中間データ（backward時に使用）
+        self.x = None
+        self.col = None
+        self.col_W = None
+
+        # 重み・バイアスパラメータの勾配
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = 1 + int((H + 2*self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2*self.pad - FW) / self.stride)
+
+        # 入力, フィルター共に2次元へ変換
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        # reshape(FN, -1) で (FN, C, FH, FW) が (FN, C*FH*FW) に変換される
+        col_W = self.W.reshape(FN, -1).T
+
+        out = np.dot(col, col_W) + self.b
+        # transposeで (N, H, W, C) の並びを (N, C, H, W) へ変換
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        # 出力を (N, C, H, W) から（N, H, W, C） へ変換し
+        # (H*W*C, FN) に変換?
+        dout = dout.transpose(0, 2, 3, 1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
 
         return dx
