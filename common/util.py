761ba2ef36cf1a9bec63d962dc901f6ef83b2200
ishijimaitsuki <tatsuki445@gmail.com>
2020-10-03 18:08:34 +0900

    im2col

--- common/util.py
+++ common/util.py
@@ -24,3 +24,30 @@ def shuffle_dataset(x, t):
     t = t[permutation]
 
     return x, t
+
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    """ 4次元から2次元への変換
+    :param input_data: 入力データ（データ数、チャンネル、高さ、幅）の４次元
+    :param filter_h: フィルターの高さ
+    :param filter_w: フィルターの幅
+    :param stride: ストライド
+    :param pad: パディング
+    :return: 2次元配列
+    """
+    N, C, H, W = input_data.shape
+    out_h = (H + 2 * pad - filter_h) // stride + 1
+    out_w = (W + 2 * pad - filter_w) // stride + 1
+
+    img = np.pad(input_data, [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
+    col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride * out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
+    return col
+
