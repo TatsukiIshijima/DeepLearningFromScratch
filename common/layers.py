5b1c73d4ff412f246b04b614541c0062325650b3
ishijimaitsuki <tatsuki445@gmail.com>
2020-10-03 21:42:04 +0900

    CNNの実装

--- common/layers.py
+++ common/layers.py
@@ -301,7 +301,7 @@ class Pooling:
         pool_size = self.pool_h * self.pool_w
         dmax = np.zeros((dout.size, pool_size))
         dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
-        dmax = dmax.reshape(dout.shape + (pool_size))
+        dmax = dmax.reshape(dout.shape + (pool_size,))
 
         dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
         dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
